b48034335a528ce3222b48473a41246000c2d990
facefamousee <facefamousee@gmail.com>
2025-06-22 11:23:37 -0400

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const a = document.getElementById("div-images")
 
-const b = ["05202025_gemini.jpeg", "05212025_prayer_brunch.JPG", "06042025_drip3.JPG", "06042025_rowan_t_bday.jpg", "06122025_celebration_concert.jpg", "06122025_jtt_fishfry.jpg", "06192025_united_we_stand.jpg", "07162025_ladybug.jpg"]
+const b = ["05272025_convention.jpg", "06042025_drip3.JPG", "06042025_rowan_t_bday.jpg", "06122025_celebration_concert.jpg", "06122025_jtt_fishfry.jpg", "06192025_united_we_stand.jpg", "07162025_ladybug.jpg"]
 
 b.forEach(element => {a.innerHTML += `<img class="images" src="assets/images/${element}"/>`});
